9dcd2baec3152ef72a9ad3e2d4781757222397eb
Bakar31 <abubakar1808031@gmail.com>
2021-08-31 12:19:34 +0600

    extracting text

---
+++ src/extract text.py
@@ -0,0 +1,10 @@
+# required libraries
+import os
+import pandas as pd
+import numpy as np
+from pdfminer import high_level
+
+#paths
+train_path = "dataset/trainResumes/"
+test_path = "dataset/testResumes/"
+
