277110ea606781b49c0724d140e200f91356bfa7
Bakar31 <abubakar1808031@gmail.com>
2021-09-01 15:59:43 +0600

    processing

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,38 @@
-from extract_text import train_resumes, test_resumes
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+nlp = spacy.load('en_core_web_sm')
 
-print(train_resumes[0])
+needless_words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+'V', 'W', 'X', 'Y', 'Z']
+punctuations = list('''!()-[]{};:'"\,<>./?@#$%^&*_~''')
+
+def text_processing(resume):   
+
+    token_list = []
+    for token in resume:
+        token_list.append(token.text)
+
+    filtered_sentence =[] 
+    for word in token_list:
+        lexeme = nlp.vocab[word]
+        if lexeme.is_stop == False:
+            filtered_sentence.append(word) 
+
+    # further filter
+    filtered_sentence_2 = []
+    for word in filtered_sentence:
+        if word not in needless_words:
+            filtered_sentence_2.append(word)
+
+    filtered_sentence_3 = []
+    for word in filtered_sentence_2:
+        if word not in punctuations:
+            filtered_sentence_3.append(word)
+    
+    Stem_words = []
+    sentence = ' '.join(filtered_sentence_3)
+    doc = nlp(sentence)
+    for word in doc:
+        Stem_words.append(word.lemma_)
+    return Stem_words
+
