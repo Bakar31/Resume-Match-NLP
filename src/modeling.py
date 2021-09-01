f1e0207b60f6a18ec67cab391a4ef468849b0536
Bakar31 <abubakar1808031@gmail.com>
2021-09-01 21:34:11 +0600

    modeling

---
+++ src/modeling.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow.keras import layers
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from preparing_to_train import train_df, test_df
+
+max_vocab_length = 10000
+max_length = 100
+
+x = train_df["resumes"].to_numpy()
+y = train_df["Match Percentage"].to_numpy()
+test_sentences = test_df["resumes"].to_numpy()
+
+text_vectorizer = TextVectorization(max_tokens=None,
+                                    standardize="lower_and_strip_punctuation",
+                                    split="whitespace", 
+                                    ngrams=None,
+                                    output_mode="int",
+                                    output_sequence_length=None)
+
+
+
+text_vectorizer = TextVectorization(max_tokens=max_vocab_length,
+                                    output_mode="int",
+                                    output_sequence_length=max_length)
+    
+text_vectorizer.adapt(x)
+text_vectorizer.adapt(test_sentences)
+
+embedding = layers.Embedding(input_dim=max_vocab_length,
+                             output_dim=128, 
+                             embeddings_initializer="uniform",
+                             input_length=max_length)
+
+embedding(text_vectorizer(x))
+embedding(text_vectorizer(test_sentences))
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+
+model = Pipeline([
+                    ("tfidf", TfidfVectorizer()), 
+                    ("reg", RandomForestRegressor())
+])
+
+model.fit(x, y)
