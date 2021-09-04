fb8d6524e42ea9739a55ad637de6ada7a6a094c5
Bakar31 <abubakar1808031@gmail.com>
2021-09-04 14:35:06 +0600

    manual feature extraction

---
+++ src/Manual feature Extraction approch/model.py
@@ -0,0 +1,14 @@
+from features import train_df, test_df
+import pandas as pd
+
+train_df.drop('resumes', axis = 1, inplace=True)
+test_df.drop('resumes', axis = 1, inplace=True)
+
+x = train_df.drop('Match Percentage', axis = 1)
+y = train_df['Match Percentage']
+
+from sklearn.ensemble import GradientBoostingRegressor
+gbr_reg = GradientBoostingRegressor(n_estimators=1000, 
+                                    learning_rate=0.01, 
+                                    max_depth=1, 
+                                    random_state=31).fit(x, y)
