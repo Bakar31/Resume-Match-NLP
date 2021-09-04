8fd409d68598c9948c49d868958507a9bf0bcdd7
Bakar31 <abubakar1808031@gmail.com>
2021-09-04 22:30:03 +0600

    tfidf

--- src/embedding/model.py
+++ src/embedding/model.py
@@ -3,8 +3,18 @@ import pandas as pd
 from tfidf import tfidf_matrix_train, tfidf_matrix_test, train_df
 
 y = train_df['Match Percentage']
-rf = RandomForestRegressor().fit(tfidf_matrix_train, y)
+#rf = RandomForestRegressor().fit(tfidf_matrix_train, y)
 
+from sklearn.ensemble import GradientBoostingRegressor
+'''gbr_reg = GradientBoostingRegressor(n_estimators=1000, 
+                                    learning_rate=0.01, 
+                                    max_depth=1, random_state=31).fit(tfidf_matrix_train, y)'''
+
+import xgboost as XGB
+xgb = XGB.XGBRegressor(learning_rate=0.01, 
+                        n_estimators=1000, 
+                        objective='reg:squarederror', 
+                        random_state = 31).fit(tfidf_matrix_train, y)
 
 def submission(model, test_sentences):
     test1 = pd.read_csv('dataset/test.csv')
@@ -13,6 +23,6 @@ def submission(model, test_sentences):
     sub_df = pd.concat([test1, prediction], axis = 1)
     return sub_df
 
-sub = submission(rf, tfidf_matrix_test)
-sub.to_csv('submission file/Submission-5.csv')
+sub = submission(xgb, tfidf_matrix_test)
+sub.to_csv('submission file/Submission.csv')
 print(sub.head())
