244150d4afad2d937c10435afff75cb95f826e7a
Bakar31 <abubakar1808031@gmail.com>
2021-09-04 16:10:05 +0600

    gbr model

--- src/Manual feature Extraction approch/model.py
+++ src/Manual feature Extraction approch/model.py
@@ -7,7 +7,4 @@ x = train_df.drop('Match Percentage', axis = 1)
 y = train_df['Match Percentage']
 
 from sklearn.ensemble import GradientBoostingRegressor
-gbr_reg = GradientBoostingRegressor(n_estimators=1000, 
-                                    learning_rate=0.01, 
-                                    max_depth=1, 
-                                    random_state=31).fit(x, y)
+gbr_reg = GradientBoostingRegressor().fit(x, y)
--- src/Manual feature Extraction approch/prediction_and_submission.py
+++ src/Manual feature Extraction approch/prediction_and_submission.py
@@ -1,6 +1,6 @@
 import pandas as pd
-#from modeling import model, test_sentences
-from modeling import gbr_reg, WordFeatures_test
+from model import gbr_reg, test_df
+
 def submission(model, test_sentences):
     test1 = pd.read_csv('dataset/test.csv')
     preds = model.predict(test_sentences)
@@ -8,6 +8,6 @@ def submission(model, test_sentences):
     sub_df = pd.concat([test1, prediction], axis = 1)
     return sub_df
 
-sub = submission(gbr_reg, WordFeatures_test)
-sub.to_csv('submission file/Submission-12.csv')
+sub = submission(gbr_reg, test_df)
+sub.to_csv('submission file/Submission-3.csv')
 print(sub.head())
