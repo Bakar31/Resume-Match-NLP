7df955f8273fc0f84f51149b8276148af37a8b72
Bakar31 <abubakar1808031@gmail.com>
2021-09-04 14:29:50 +0600

    update

--- src/model.py
+++ src/model.py
@@ -7,18 +7,18 @@ test_df.drop('resumes', axis = 1, inplace=True)
 x = train_df.drop('Match Percentage', axis = 1)
 y = train_df['Match Percentage']
 
-'''from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 gbr_reg = GradientBoostingRegressor(n_estimators=1000, 
                                     learning_rate=0.01, 
                                     max_depth=1, 
-                                    random_state=31).fit(x, y)'''
+                                    random_state=31).fit(x, y)
 
 
 
-import xgboost as XGB
+'''import xgboost as XGB
 xgb = XGB.XGBRegressor(learning_rate=0.01, 
                         n_estimators=100, 
-                        random_state = 31).fit(x, y)
+                        random_state = 31).fit(x, y)'''
 
 
 def submission(model, test):
@@ -28,6 +28,6 @@ def submission(model, test):
     sub_df = pd.concat([test1, prediction], axis = 1)
     return sub_df
 
-sub = submission(xgb, test_df)
-sub.to_csv('submission file/Submission-5.csv')
+sub = submission(gbr_reg, test_df)
+sub.to_csv('submission file/Submission-1.csv')
 print(sub.head())
