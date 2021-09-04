73546750678f904dd20f298184cbe13a9639fc79
Bakar31 <abubakar1808031@gmail.com>
2021-09-04 14:25:57 +0600

    preparing to train

--- src/preparing_to_train.py
+++ src/preparing_to_train.py
@@ -1,25 +1,22 @@
 import pandas as pd
-from extract_text import train_resumes, test_resumes
+from extract_text import train_resumes, test_resumes, train, test
 from processing import text_processing
 
 processed_resumes_train = []
 processed_resumes_test = []
 
-for  resume in train_resumes:
+for resume in train_resumes:
     processed_resume = text_processing(resume)
     processed_resumes_train.append(processed_resume)
 
-for  resume in test_resumes:
+for resume in test_resumes:
     processed_resume = text_processing(resume)
     processed_resumes_test.append(processed_resume)
 
-'''print(len(processed_resumes_train))
-print(len(processed_resumes_test))'''
-#print(processed_resumes_train[1])
-#print(processed_resumes_test[0])
-
-train = pd.read_csv('dataset/train.csv')
-test = pd.read_csv('dataset/test.csv')
+print(len(processed_resumes_train))
+print(len(processed_resumes_test))
+print(processed_resumes_train[1])
+print(processed_resumes_test[0])
 
 def dataframe(resume_list, df):
     resumes =  pd.DataFrame(resume_list, columns = ['resumes'])
@@ -30,5 +27,5 @@ def dataframe(resume_list, df):
 train_df = dataframe(processed_resumes_train, train)
 test_df = dataframe(processed_resumes_test, test)
 
-'''print(train_df.head())
-print(test_df.head())'''
+print(train_df.head())
+print(test_df.head())
