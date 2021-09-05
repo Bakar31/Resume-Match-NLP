3e9f70ead8fe72a49d05e1cf8e520537eabf7f21
Bakar31 <abubakar1808031@gmail.com>
2021-09-05 21:10:45 +0600

    tuned params

--- src/Manual feature Extraction approch/model.py
+++ src/Manual feature Extraction approch/model.py
@@ -1,3 +1,4 @@
+import xgboost as XGB
 from features import train_df, test_df
 
 train_df.drop('resumes', axis = 1, inplace=True)
@@ -6,3 +7,13 @@ test_df.drop('resumes', axis = 1, inplace=True)
 x = train_df.drop('Match Percentage', axis = 1)
 y = train_df['Match Percentage']
 
+# xgboost regressior
+xgb = XGB.XGBRegressor(learning_rate=0.005, 
+                        n_estimators=700, 
+                        objective='reg:squarederror', 
+                        max_depth=8, 
+                        reg_lambda = 1.3,
+                        gamma = 1,
+                        min_child_weight =1.5,
+                        max_delta_step = 100,
+                        random_state = 31).fit(x, y)
