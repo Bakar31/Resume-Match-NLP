60983debae566602d52bafbf8c5d0841e3a7ae10
Bakar31 <abubakar1808031@gmail.com>
2021-09-06 13:37:38 +0600

    countvect implement

---
+++ src/Countvector Approch/countvec.py
@@ -0,0 +1,26 @@
+# required libraries
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from processing import train_lemma, test_lemma
+
+# datasets
+train = pd.read_csv('dataset/train.csv')
+test = pd.read_csv('dataset/test.csv')
+
+# preprocessed and final dataset dataset
+train_df = pd.concat([train, pd.DataFrame(train_lemma, columns=['resumes'])], axis = 1)
+test_df = pd.concat([test, pd.DataFrame(test_lemma, columns=['resumes'])], axis = 1)
+
+print(train_df.head())
+print(test_df.head())
+
+# apply TFIDF
+countvec = CountVectorizer(analyzer='word',
+                        token_pattern=r'\w{1,}',
+                        ngram_range=(1, 3), 
+                        stop_words = 'english')
+
+countvec_matrix_train = countvec.fit_transform(train_df['resumes'])
+countvec_matrix_test = countvec.transform(test_df['resumes'])
+print(countvec_matrix_train.shape)
+print(countvec_matrix_test.shape)
